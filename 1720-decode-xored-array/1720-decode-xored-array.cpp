185e696327ad37205dba64f91621ad57b533e833
Ayush Jha <97244608+Ayush0054@users.noreply.github.com>
2022-08-15 23:46:12 +0530

    Time: 65 ms (16.05%), Space: 26 MB (36.42%) - LeetHub

---
+++ 1720-decode-xored-array/1720-decode-xored-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+         vector<int> res;
+        res.push_back(first);
+        for(int i=0;i<encoded.size();i++){
+            int ans = res[i] ^ encoded [i];
+            res.push_back(ans);
+            
+        }
+        return res;
+    }
+};
