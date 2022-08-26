8240d261c53a4b9d313bad4b62e8ceeebd99637a
Ayush Jha <97244608+Ayush0054@users.noreply.github.com>
2022-08-26 18:16:22 +0530

    Time: 10 ms (75.77%), Space: 7.7 MB (37.19%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -7,43 +7,39 @@
  * };
  */
 class Solution {
-    private: 
-    ListNode* fdetectnode(ListNode* head){
-    if(head==NULL){
-        return NULL;
-
-    }
-    ListNode* fast = head;
-    ListNode* slow = head;
-    while(slow !=NULL && fast != NULL){
-        fast = fast -> next;
-        if(fast != NULL ){
-            fast = fast -> next;
-        }
-        slow = slow -> next;
-
-        if(slow==fast){
-            return slow;
-        }
-
-    }
-    return NULL;
-}
+    private:
+     ListNode* fdetection(ListNode *head){
+         if(head==NULL){
+             return NULL;
+         }
+         ListNode* fast = head;
+         ListNode* slow = head;
+             while(slow!=NULL && fast!=NULL){
+                 fast = fast -> next;
+                 if(fast != NULL){
+                   fast = fast -> next;
+                 }
+                 slow = slow -> next;
+                 if(slow==fast){
+                     return slow;
+                 }
+             }
+         return NULL;
+     }
 public:
     ListNode *detectCycle(ListNode *head) {
         if(head==NULL){
-        return NULL;
-    }
-
-    ListNode* intersection = fdetectnode(head);
-    if(intersection == NULL){
-        return NULL;
-    }
-    ListNode* slow = head;
-    while(slow!= intersection){
-        slow = slow -> next;
-        intersection = intersection -> next;
-    }
-    return slow;
+             return NULL;
+         }
+        ListNode* intersection = fdetection(head);
+        if(intersection == NULL){
+            return NULL;
+        }
+        ListNode* slow = head;
+        while(slow!=intersection){
+          intersection = intersection -> next;
+            slow = slow -> next;
+        }
+        return slow;
     }
 };
