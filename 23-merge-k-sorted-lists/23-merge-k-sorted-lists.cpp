ffb1500b1776425c45a04a19356ed45f9c31ac20
Ayush Jha <97244608+Ayush0054@users.noreply.github.com>
2022-09-06 21:43:35 +0530

    Time: 223 ms (23.39%), Space: 13.4 MB (48.34%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -10,36 +10,64 @@
  */
 class Solution {
 public:
-    struct comp{
-        bool operator()(const ListNode* l, ListNode* r){
-            return l->val > r->val;
+    ListNode* mergetwo(ListNode* list1,ListNode* list2){
+         if(list1==NULL){
+             return list2;
+         }
+         if(list2==NULL){
+             return list1;
+         }
+        if(list1->val < list2->val){
+            list1 -> next = mergetwo(list1->next,list2);
+            return list1;
         }
-    };
+        else{
+            list2->next = mergetwo(list1,list2->next);
+            return list2;
+        }
+    }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<ListNode*,vector<ListNode*>,comp>q;
+        ListNode* result = NULL;
         
-            for(int i=0;i<lists.size();i++){
-                ListNode* temp = lists[i];
-                if(temp!=NULL){
-                    q.push(temp);
-                }
-            }  
-        
-        ListNode* dummy = new ListNode(0);
-        ListNode* curr = dummy;
-        
-        while(q.size()>0){
-            ListNode* temp = q.top();
-            q.pop();
-            
-            curr ->next = temp;
-            curr = curr->next;
-            
-            if(temp->next!=NULL){
-                q.push(temp->next);
-            }
+        for(int i=0;i<lists.size();i++){
+            result = mergetwo(result,lists[i]);
         }
-        return dummy-> next;
-             
+        return result;
     }
 };
+     // soln 2
+// class Solution {
+// public:
+//     struct comp{
+//         bool operator()(const ListNode* l, ListNode* r){
+//             return l->val > r->val;
+//         }
+//     };
+//     ListNode* mergeKLists(vector<ListNode*>& lists) {
+//         priority_queue<ListNode*,vector<ListNode*>,comp>q;
+        
+//             for(int i=0;i<lists.size();i++){
+//                 ListNode* temp = lists[i];
+//                 if(temp!=NULL){
+//                     q.push(temp);
+//                 }
+//             }  
+        
+//         ListNode* dummy = new ListNode(0);
+//         ListNode* curr = dummy;
+        
+//         while(q.size()>0){
+//             ListNode* temp = q.top();
+//             q.pop();
+            
+//             curr ->next = temp;
+//             curr = curr->next;
+            
+//             if(temp->next!=NULL){
+//                 q.push(temp->next);
+//             }
+//         }
+//         return dummy-> next;
+             
+//     }
+// };
