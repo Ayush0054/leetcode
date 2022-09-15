90bb1c0e651228d44237aa6bec9bb31debebbaf6
Ayush Jha <97244608+Ayush0054@users.noreply.github.com>
2022-09-15 17:57:17 +0530

    Time: 2059 ms (18.30%), Space: 294.8 MB (20.95%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -29,35 +29,36 @@ class Solution {
     }
 public:
     ListNode* deleteMiddle(ListNode* head) {
-//         if(head==NULL||head->next == NULL){
-//             return NULL;
-//         }
-//         if(head->next->next==NULL){
-//             head -> next = NULL;
-//             return head;
-//         }
-//          pair<ListNode*,ListNode*> p = middlenode(head);
-//         ListNode* prev= p.first;
-//         ListNode* middle = p.second;
-        
-//         prev -> next = middle -> next;
-//         middle -> next = NULL;
-//         delete middle;
-        
-//         return head;
-        
-          ListNode*slow=head;
-        ListNode*fast=head;
-        ListNode*prev=slow;
-        if(head->next==NULL)return NULL;
-        while(fast&&fast->next)
-        {
-            prev=slow;
-            slow=slow->next;
-            fast=fast->next->next;
+        if(head==NULL||head->next == NULL){
+            return NULL;
         }
-        prev->next=slow->next;
-        delete(slow);
+        if(head->next->next==NULL){
+            head -> next = NULL;
+            return head;
+        }
+         pair<ListNode*,ListNode*> p = middlenode(head);
+        ListNode* prev= p.first;
+        ListNode* middle = p.second;
+        
+        prev -> next = middle -> next;
+        middle -> next = NULL;
+        delete middle;
+        
         return head;
+        
+        //aproach 2
+        //   ListNode*slow=head;
+        // ListNode*fast=head;
+        // ListNode*prev=slow;
+        // if(head->next==NULL)return NULL;
+        // while(fast&&fast->next)
+        // {
+        //     prev=slow;
+        //     slow=slow->next;
+        //     fast=fast->next->next;
+        // }
+        // prev->next=slow->next;
+        // delete(slow);
+        // return head;
     }
 };
