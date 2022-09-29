2a619b2cce936aec3ffaf0bea7789905096759cd
Ayush Jha <97244608+Ayush0054@users.noreply.github.com>
2022-09-29 14:03:55 +0530

    Time: 20 ms (66.40%), Space: 5.9 MB (30.85%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int a=0;
+        if(x<0 || (x!=0 &&x%10==0)) return false;
+       while(x>a){
+           a = a*10+x%10;
+           x = x/10;
+       }
+        return x==a || x==a/10;
+    }
+};
