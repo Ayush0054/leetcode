c42c17cb35483d76c36065696b2a3e7dadde598d
Ayush Jha <97244608+Ayush0054@users.noreply.github.com>
2022-10-27 21:34:30 +0530

    Time: 124 ms (5.04%), Space: 175.4 MB (5.79%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -18,5 +18,25 @@ public:
             }
         }
         return ans;
+        
+        /***********************
+       // second approach 
+        string ans="";
+        int opened = 0;
+        for (char c : s) {
+            if (c == '(' && opened++ > 0){               
+                ans += c;
+            }
+            if (c == ')' && opened-- > 1){           
+                ans += c;
+            }
+        }
+        return ans;
+        
+        *****************/
+        
+        
+        
+        
     }
 };
