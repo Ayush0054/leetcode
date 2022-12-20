a6678e4a699d843da342986c09b1d6816370e9a1
Ayush Jha <97244608+Ayush0054@users.noreply.github.com>
2022-12-20 21:40:54 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (91.74%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,18 +11,67 @@
  */
 class Solution {
 public:
-     void inorder(TreeNode* root,vector<int>&v){ 
-       
-         if(root==NULL){
-            return;
-        }
-        inorder(root->left,v);
-        v.push_back(root->val);
-        inorder(root->right,v);
-     }
+     // void inorder(TreeNode* root,vector<int>&v){ 
+        // recursive
+         
+    //      if(root==NULL){
+    //         return;
+    //     }
+    //     inorder(root->left,v);
+    //     v.push_back(root->val);
+    //     inorder(root->right,v);
+    //  }
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int>s;
-         inorder(root,s);
-        return s;
+    //     vector<int>s;
+    //      inorder(root,s);
+    //     return s;
+         
+         // iterative
+    
+//          vector<int> v;
+//          stack<TreeNode*>a;
+//          while(root || !a.empty()){
+//           while(root){
+//               a.push(root);
+//               root = root -> left;
+              
+//           }  
+//              root = a.top();
+//              a.pop();
+//              v.push_back(root->val);
+//              root = root -> right;
+//          }
+//          return v;
+        
+        ////////////////////////morris/////////////
+         vector<int> ans;
+         if(root==NULL){
+            return ans;
+        }
+       TreeNode* current;
+            TreeNode* pre;
+             current = root;
+        while(current!=NULL){
+            if(!current->left){
+            ans.push_back(current->val);
+                current=current->right;
+            }
+            else{
+                 pre=current->left;
+                while(pre->right != NULL && pre->right != current){
+                    pre=pre->right;
+               }
+                    if(pre->right==NULL){
+                        pre->right=current;
+                        current=current->left;
+                    }
+                    else{
+                        pre->right=NULL;
+                        ans.push_back(current->val);
+                        current=current->right;
+                    }
+                }
+        }
+        return ans;
     }
 };
