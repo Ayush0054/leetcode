1ae8ad038aa67669c826102522f57a0e7d7d503a
Ayush Jha <97244608+Ayush0054@users.noreply.github.com>
2022-12-24 17:09:41 +0530

    Time: 12 ms (88.13%), Space: 21.7 MB (40.01%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -10,28 +10,52 @@
  * };
  */
 class Solution {
-public: void inorder(TreeNode* root,vector<int>& res){
+ public: 
+    // void inorder(TreeNode* root,vector<int>& res){
 
-       if(root==NULL){
-           return;
-       }
-       inorder(root->left,res);
-          res.push_back(root->val); 
-       inorder(root->right,res);
-}
+//        if(root==NULL){
+//            return;
+//        }
+//        inorder(root->left,res);
+//           res.push_back(root->val); 
+//        inorder(root->right,res);
+// }
     bool isValidBST(TreeNode* root) {
+//         if(root==NULL){
+//             return true;
+//         }
+//         vector<int>ans;
+//         inorder(root,ans);
+//         for(int i=1;i<ans.size();i++){
+//             // cout<<ans[i]<<" ";
+//             if(ans[i-1]>=ans[i]){
+//               return false;
+//             }
+            
+//         }
+//         return true;
+        
+        return valid(root,LONG_MIN,LONG_MAX);
+    }
+    bool valid(TreeNode* root,long min,long max){
         if(root==NULL){
             return true;
         }
-        vector<int>ans;
-        inorder(root,ans);
-        for(int i=1;i<ans.size();i++){
-            // cout<<ans[i]<<" ";
-            if(ans[i-1]>=ans[i]){
-              return false;
-            }
-            
+    
+//        else if(root!=NULL && root->right==NULL && root->left==NULL){
+//              return false;
+//          }
+             
+//         // if(root->right==NULL || root->left==NULL){
+//         //      return false;
+//         //  }
+        if(root->val>min && root->val<max){
+            bool left=valid(root->left,min,root->val);
+            bool right=valid(root->right,root->val,max);
+            return left && right ;
         }
-        return true;
+        else{
+            return false;
+        }
     }
 };
